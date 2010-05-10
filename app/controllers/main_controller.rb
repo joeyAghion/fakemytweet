3bf69e0f9398922f4876aedbf61b4f2b8ec2452c
Joey Aghion <joey@aghion.com>
2010-05-10 00:59:48 -0400

    hook up try again/tweet this buttons

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,7 +5,14 @@ class MainController < ApplicationController
   end
   
   def create
-    @tweeter.load_tweets if @tweeter.valid?
+    begin
+      @tweeter.load_tweets if @tweeter.valid?
+    rescue Timeout::Error => ex
+      flash.now[:notice] = "We didn't hear back from Twitter in time. Try again later."
+    rescue => ex
+      flash.now[:notice] = 'Whoops - There was a problem. Try again later.'
+    end
+    
     render :action => 'index'
   end
   
