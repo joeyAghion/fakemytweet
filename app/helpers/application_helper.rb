778e301ef6c945f6006a0ebc4b2c7d42f2710705
Joey Aghion <joey@aghion.com>
2010-05-27 11:47:11 -0400

    add dynamic page title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def page_title
+    "#{@tweeter.screen_name + " - " if @tweeter && @tweeter.screen_name.present?}Fake My Tweet"
+  end
 end
