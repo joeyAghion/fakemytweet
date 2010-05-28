f18329928e706934393f7dd08ee17399a1229216
Joey Aghion <joey@aghion.com>
2010-05-28 00:51:02 -0400

    commit exception-raising statement to test hoptoad notification

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -8,6 +8,7 @@ class MainController < ApplicationController
   def index
     if params[:screen_name].present?
       begin
+        a = 23/0
         if @tweeter.valid?
           if tweets = @tweeter.load_tweets
             if tweets.empty?
